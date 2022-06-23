61dfab262f5224686ca2c131d652234b75155c79
kai <kailashjat604@gmail.com>
2022-06-23 09:52:02 +0530

    Removed database file

--- collegemanagement/settings.py
+++ collegemanagement/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4v2rlkef29b^t_a%w!n#t!_7igdqb33lt3(ogez11%yh-1#^r4'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
